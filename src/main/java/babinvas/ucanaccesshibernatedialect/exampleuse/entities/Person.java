bc869e15f834b61c870920322ba29b1fd1c3d60a
babinvas <babinvas@gmail.com>
2021-03-15 19:10:07 +0300

    Changes were made to the class of the person

--- src/main/java/babinvas/ucanaccesshibernatedialect/exampleuse/entities/Person.java
+++ src/main/java/babinvas/ucanaccesshibernatedialect/exampleuse/entities/Person.java
@@ -11,7 +11,6 @@ public class Person {
 
 	private String surname;
 	private String name;
-	private String patronymic;
 
 	@OneToOne(optional = false, cascade = CascadeType.ALL)
 	@JoinColumn(name = "passport_id")
@@ -22,17 +21,13 @@ public class Person {
 
 	private String email;
 
-	@Column(name = "email_2")
-	private String email2;
-
 	public Person() {
 	}
 
-	public Person(long id, String surname, String name, String patronymic) {
+	public Person(long id, String surname, String name) {
 		this.id = id;
 		this.surname = surname;
 		this.name = name;
-		this.patronymic = patronymic;
 	}
 
 	public long getId() {
@@ -59,14 +54,6 @@ public class Person {
 		this.name = name;
 	}
 
-	public String getPatronymic() {
-		return patronymic;
-	}
-
-	public void setPatronymic(String patronymic) {
-		this.patronymic = patronymic;
-	}
-
 	public Passport getPassport() {
 		return passport;
 	}
@@ -89,13 +76,5 @@ public class Person {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getEmail2() {
-		return email2;
-	}
-
-	public void setEmail2(String email2) {
-		this.email2 = email2;
 	}
 }
