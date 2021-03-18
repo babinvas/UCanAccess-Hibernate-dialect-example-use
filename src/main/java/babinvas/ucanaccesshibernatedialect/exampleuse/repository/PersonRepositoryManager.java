668b665c8ff43b13ebddf18f67c54e43bed4b3c9
babinvas <babinvas@gmail.com>
2021-03-18 19:41:07 +0300

    The person's working methods with the database have been changed

--- src/main/java/babinvas/ucanaccesshibernatedialect/exampleuse/repository/PersonRepositoryManager.java
+++ src/main/java/babinvas/ucanaccesshibernatedialect/exampleuse/repository/PersonRepositoryManager.java
@@ -1,5 +1,6 @@
 package babinvas.ucanaccesshibernatedialect.exampleuse.repository;
 
+import babinvas.ucanaccesshibernatedialect.exampleuse.entities.AddressPerson;
 import babinvas.ucanaccesshibernatedialect.exampleuse.entities.Person;
 
 import javax.persistence.EntityManager;
@@ -11,6 +12,39 @@ public class PersonRepositoryManager implements RepositoryManager<Person> {
 
 	public PersonRepositoryManager(EntityManager entityManager) {
 		this.entityManager = entityManager;
+	}
+
+	@Override
+	public void createEntity(Person person) {
+		entityManager.persist(person);
+		executeTransaction();
+	}
+
+	@Override
+	public Person readEntity(long id) {
+		Person person = entityManager.find(Person.class, id);
+
+		AddressPersonRepositoryManager addressPersonRepositoryManager = new AddressPersonRepositoryManager(entityManager);
+		AddressPerson addressPerson = addressPersonRepositoryManager.readEntity(id);
+		person.setAddress(addressPerson);
+
+		executeTransaction();
+
+		return person;
+	}
+
+	@Override
+	public void updateEntity(Person person) {
+		entityManager.merge(person);
+		executeTransaction();
+	}
+
+	@Override
+	public void deleteEntity(long id) {
+		Person person = entityManager.find(Person.class, id);
+		entityManager.remove(person);
+
+		executeTransaction();
 	}
 
 	private void executeTransaction() {
@@ -31,27 +65,5 @@ public class PersonRepositoryManager implements RepositoryManager<Person> {
 	private void close() {
 		entityManager.getEntityManagerFactory().close();
 		entityManager.close();
-	}
-
-	@Override
-	public void createEntity(Person person) {
-		entityManager.persist(person);
-		executeTransaction();
-	}
-
-	@Override
-	public Person readEntity(long id) {
-		Person person = entityManager.find(Person.class, id);
-		executeTransaction();
-
-		return person;
-	}
-
-	@Override
-	public void updateEntity(long id, Person person) {
-	}
-
-	@Override
-	public void deleteEntity(long id) {
 	}
 }
--- src/main/java/babinvas/ucanaccesshibernatedialect/exampleuse/repository/RepositoryManager.java
+++ src/main/java/babinvas/ucanaccesshibernatedialect/exampleuse/repository/RepositoryManager.java
@@ -3,6 +3,6 @@ package babinvas.ucanaccesshibernatedialect.exampleuse.repository;
 public interface RepositoryManager<T> {
 	void createEntity(T t);
 	T readEntity(long id);
-	void updateEntity(long id, T t);
+	void updateEntity(T t);
 	void deleteEntity(long id);
 }
